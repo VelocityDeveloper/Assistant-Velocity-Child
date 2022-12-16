3420138af94ada61f00b0e107fb133e7b3a45501
Velocity Developer <76415135+VelocityDeveloper@users.noreply.github.com>
2022-12-16 15:21:49 +0700

    jangan lupa js-nya

---
+++ rumahdaihatsu.com or bikinrumah.id/custom.js
@@ -0,0 +1,10 @@
+jQuery(function($) {    
+$(".tombols").click(function() {
+    $("#formsearchvel").toggle();
+    $(".tombols").toggleClass( "collapsed" );
+});
+$(".tombolsbaru").click(function() {
+    $("#formsearchvelbaru").toggle();
+    $(".tombolsbaru").toggleClass( "collapsed" );
+});
+});
